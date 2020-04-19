4754662b8286ede44cef597d0ead3595faf8f556
SANYAM <sanyamsingh.96@gmail.com>
2020-04-19 17:23:20 +0100

    covid idea

--- P_STATS.py
+++ P_STATS.py
@@ -25,3 +25,17 @@ def frequency_table(numbers):
 if __name__ == '__main__':
     scores = [2,4,3,2,6,5,66,3,2,5,66,3,2,4,2]
     frequency_table(scores)
+
+# finding the rangae
+def find_range(numbers):
+    lowest = min(numbers)
+    highest = max(numbers)
+    # find the range
+    r = highest - lowest
+    return lowest,highest,r
+if __name__ == '__main__':
+    donations = [100, 60, 70, 900, 100, 200, 500, 500, 503, 600, 1000, 1200]
+    lowest,highest,r = find_range(donations)
+    print('lowest:{0} highest :{1} range:{2}'.format(low,highest,r))
+
+
